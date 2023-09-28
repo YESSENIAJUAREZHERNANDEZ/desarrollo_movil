417c146799082ae7c80d9943524accf68ef67a3c
mediplan <1720110342@utectulancingo.edu.mx>
2023-09-27 18:52:18 -0600

    prueba

--- lib/presenter/controller/login_list_controller.dart
+++ lib/presenter/controller/login_list_controller.dart
@@ -1,5 +1,38 @@
+import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 
 class LoginPageController extends GetxController{
   final title = 'Login'.obs;
+  final formKey = GlobalKey<FormState>();
+
+  var email = ''.obs;
+  var password = ''.obs;
+
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  String? emailvalidator(String? value){
+    if (value!.isEmpty || !value.contains('@')){
+      return 'Please enter a valid email address';
+    }
+    return null;
+  }
+
+  String? passwordValidator(String? value){
+    if (value!.isEmpty || value.length <8 ){
+      return '8 CARACTERES PORFAA; must 8';
+    }
+    return null;
+  }
+
+  void submitAttempt(){
+    final isValid = formKey.currentState!.validate();
+    if(isValid){
+      Get.snackbar('Success','Login Successful');
+      Get.focusScope!.unfocus();
+      print(emailController.text);
+      print(passwordController.text);
+    }
+  }
+
 }
--- lib/presenter/views/login_page.dart
+++ lib/presenter/views/login_page.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:basicapp2/presenter/controller/login_list_controller.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends GetView<LoginPageController> {
   const LoginPage({Key? key}) : super(key: key);
 
   @override
@@ -10,9 +10,85 @@ class LoginPage extends StatelessWidget {
     return Scaffold(
         appBar: AppBar(
           automaticallyImplyLeading: false,
+          centerTitle: true,
           title: const Text('Login'),
         ),
-        body:
-        Center(child: Text(Get.find<LoginPageController>().title.value)));
+        body: SingleChildScrollView(
+        child: Center(
+            child:Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(padding: EdgeInsets.all(16),
+                child: Form(
+                  key: controller.formKey,
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        height: 120,
+                      ),
+                      Text(
+                        'Login',
+                        style:TextStyle(
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 50,
+                      ),
+                      TextFormField(
+                        validator: controller.emailvalidator,
+                        controller: controller.emailController,
+                        decoration: InputDecoration(
+                          hintText: 'Email',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                        onSaved: (value) {
+                          controller.email = value! as RxString;
+                        },
+                      ),
+                        // EL OTRO
+                      SizedBox(
+                        height: 50,
+                      ),
+                      TextFormField(
+                        validator: controller.passwordValidator,
+                        obscureText: true,
+                        controller: controller.passwordController,
+                        decoration: InputDecoration(
+                          hintText: 'Password',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                        onSaved: (value){
+                          controller.password = value! as RxString;
+                        },
+                      ),
+                      SizedBox(
+                        height: 50,
+                      ),
+                      Container(
+                        height: 50,
+                        width: 150,
+                        child: MaterialButton(
+                          color: Colors.cyanAccent,
+                          onPressed: (){} ,
+                          child: Text('Login'),
+                        )
+                      ),
+                    ],
+                  ),
+                ),
+                )
+              ],
+            )
+          ),
+        ),
+    );
+        //Center(child: Text(Get.find<LoginPageController>().title.value)));
+
   }
 }
